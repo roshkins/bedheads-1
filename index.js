f01caedb91665f20040058df1665f6cae15d70cf
joshua sorkin <joshua.d.sorkin@gmail.com>
2018-02-03 14:49:02 -0800

    adding twilio test

--- index.js
+++ index.js
@@ -1,10 +1,70 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app=express();
+const VoiceResponse=require('twilio').twiml.VoiceResponse;
+//const MessagingResponse=require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
+const client=require('twilio')(
+	process.env.BEDHEADS_TWILIO_ACCOUNT_SID,
+	process.env.BEDHEADS_TWILIO_AUTH_TOKEN
+);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var https=require("https");
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+const port=process.env.PORT;
+app.listen(port,()=>{
+	console.log('live on port '+port);
+});
+
+
+app.post('/voice',(req,res)=>{
+	
+	console.log("test phone output");
+	//const response=new VoiceResponse();
+	//response.say("Listing facilities now.");
+	//sendResponse(response,res);
+			
+});
+
+
+/*
+app.get('/handleMainMenuResponse',(req,res)=>{
+	var digits=req.query.Digits;
+	const response=new VoiceResponse();
+	
+	switch(digits){
+		case "1":
+			response.say("Listing facilities now.");
+			break;
+		case "2":
+			response.say("")
+	}
+	if (digits=="1"){
+		response.say("Listing facilities now.");
+	}
+	elseif (digits=="2"){
+		response.say("You didn't press 1.");
+	}
+	responseTwiml=response.toString();
+	console.log("responseTwiml: "+responseTwiml);
+	res.send(responseTwiml);
+});
+*/
+
+function sendResponse(response,res){
+	responseTwiml=response.toString();
+	console.log("responseTwiml: "+responseTwiml);
+	res.send(responseTwiml);
+}
+
+//creates a url from an array of key-value pairs
+function buildGetUrl(baseUrl,paramArray){
+	url=baseUrl+"?";
+	Object.keys(paramArray).forEach(function(key){
+		url+=key+"="+encodeURIComponent(paramArray[key])+"&";
+	});
+	url=url.slice(0,-1);
+	return url;
+	
+}
